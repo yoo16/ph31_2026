3859c220fe3b40eb28eeedf8c8c7910a2cfe004a
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-13 15:06:59 +0900

    update

--- 05_variable/superglobals.php
+++ 05_variable/superglobals.php
@@ -13,8 +13,8 @@ $name = $_GET['name'] ?? 'ゲスト';
 
 // TODO: $_POST: フォームから送信されたデータの取得
 // フォームから送信された 'user_name' と 'message' を取得し、なければ空文字をデフォルト値
-$postedMessage =  '';
-$postedName = '';
+$postedMessage =  $_POST['message'] ?? '';
+$postedName = $_POST['user_name'] ?? '';
 
 // TODO: $_SERVER: サーバーや実行環境の情報
 // サーバー名:$_SERVER['SERVER_NAME']
@@ -22,7 +22,7 @@ $serverName = "";
 // ユーザーエージェント: $_SERVER['HTTP_USER_AGENT']
 $userAgent = "";
 // リクエスト方法: $_SERVER['REQUEST_METHOD']
-$requestMethod = "";
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 // クライアントのIPアドレス: $_SERVER['REMOTE_ADDR']
 $remoteAddr = "";
 ?>
