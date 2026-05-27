62bb2186e88b2cc239f52981b6d418297b3d2736
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-05-27 16:15:37 +0900

    update

--- 11_function/data_check.php
+++ 11_function/data_check.php
@@ -35,8 +35,8 @@ $results['string'] = [
 // 3. 部分文字列の抽出 (substr)
 // TODO: substr, mb_substr関数を使用して、文字列の一部を抽出
 $results['substr'] = [
-    'substr_5' => "",   // 先頭から5文字
-    'mb_substr_2' => "",  // 先頭から2文字（マルチバイト対応）
+    'substr_5' => substr($trimmedString, 0, 5),   // 先頭から5文字
+    'mb_substr_2' => mb_substr($trimmedString, 0, 2),  // 先頭から2文字（マルチバイト対応）
 ];
 
 // 4. 置換・検索
