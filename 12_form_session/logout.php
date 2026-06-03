0b7bc7961edb3412e1925fb6c5d62aa8f70a099c
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-03 16:19:44 +0900

    update

--- 12_form_session/logout.php
+++ 12_form_session/logout.php
@@ -10,11 +10,9 @@ session_start();
 
 // セッション変数の破棄
 // TODO: セッション変数の破棄: unset() authUser, message, status
-// if (isset($_SESSION['authUser'])) {
-    // unset($_SESSION['authUser']);
-    // unset($_SESSION['message']);
-    // unset($_SESSION['status']);
-// }
+if (isset($_SESSION['authUser'])) {
+    unset($_SESSION['authUser']);
+}
 
 // ログインページへリダイレクト
 header('Location: post_request.php');
