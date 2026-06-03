520e580aa0dc8b526eb9eaf28465130544a399b3
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-03 17:35:33 +0900

    update

--- 13_datetime/calendar.php
+++ 13_datetime/calendar.php
@@ -66,7 +66,7 @@ $next_month_date->modify('+1 month');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?> | PHP Samples</title>
+    <title>PHP Samples</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../css/style.css">
     <style>
